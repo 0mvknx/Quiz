f315615cbc6bb5c309d250073a9877d5988fe648
Marcos Vinicios <mva.knx@gmail.com>
2022-06-06 23:57:58 -0300

    Alterar estrutura scafold
    Alterar estrutura scafold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'core/app_widget.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,13 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return AppWidget();
   }
 }
 
