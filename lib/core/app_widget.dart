dd4b2d55611cda7ed13c072ebdbcd1c62eb173fc
Marcos Vinicios <mva.knx@gmail.com>
2022-06-09 00:32:30 -0300

    update

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/splash/splash.page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "DevQuiz",
-      home: Scaffold(),
+      home: SplashPage(),
     );
   }
 }
---
+++ lib/splash/splash.page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/core/app_gradients.dart';
+import 'package:quiz/core/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
+  }
+}
