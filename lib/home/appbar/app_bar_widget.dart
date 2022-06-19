d5768ed63b1a5ee12305629531d26e8a4de98bd1
Marcos Vinicios <mva.knx@gmail.com>
2022-06-19 19:16:57 -0300

    atualizacao projeto

--- lib/home/appbar/app_bar_widget.dart
+++ lib/home/appbar/app_bar_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/core/app_gradients.dart';
 import 'package:quiz/core/app_text_styles.dart';
+import 'package:quiz/home/appbar/score_card/score_card_widget.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
@@ -11,30 +12,36 @@ class AppBarWidget extends PreferredSize {
             decoration: BoxDecoration(gradient: AppGradients.linear),
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Column(
                 children: [
-                  Text.rich(
-                    TextSpan(
-                        text: ("Olá, "),
-                        style: AppTextStyles.title,
-                        children: [
-                          TextSpan(
-                              text: ("Marcos Vinicios."),
-                              style: AppTextStyles.titleBold),
-                        ]),
-                  ),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://avatars.githubusercontent.com/u/107013353?v=4"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                            text: ("Olá, "),
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                  text: ("Marcos Vinicios."),
+                                  style: AppTextStyles.titleBold),
+                            ]),
                       ),
-                    ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/107013353?v=4"),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+                  // ignore: prefer_const_constructors
+                  ScoreCardWidget(),
                 ],
               ),
             ),
